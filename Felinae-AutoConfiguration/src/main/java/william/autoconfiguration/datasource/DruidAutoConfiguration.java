4a8600b486e6a2ea8fd6d49911f60fb64bc24e5e
zhangshenao <zhangshenao@zhihu.com>
2018-07-09 23:23:58 +0800

    Add Druid Auto-Configuration

---
+++ Felinae-AutoConfiguration/src/main/java/william/autoconfiguration/datasource/DruidAutoConfiguration.java
@@ -0,0 +1,69 @@
+package william.autoconfiguration.datasource;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Auther: ZhangShenao
+ * @Date: 2018/7/5 13:50
+ * @Description: Druid数据源自动配置
+ */
+@Configuration
+@EnableConfigurationProperties(DruidStatProperties.class)
+@ConditionalOnClass(DruidDataSource.class)
+@ConditionalOnProperty(name = "spring.datasource.type",havingValue = "com.alibaba.druid.pool.DruidDataSource")
+public class DruidAutoConfiguration {
+    @Autowired
+    private DruidStatProperties druidStatProperties;
+
+    //注册Druid数据源,并绑定spring.datasource配置的数据源属性
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @ConditionalOnMissingBean(DruidDataSource.class)
+    public DataSource druidDataSource(){
+        return new DruidDataSource();
+    }
+
+    //配置Druid后台监控的Servlet
+    @Bean
+    @ConditionalOnProperty(name = "druid.stat.active")
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(
+                    new StatViewServlet(),druidStatProperties.getServletUrlMappings());
+        Map<String,String> initParams = new HashMap<>();
+        initParams.put("loginUsername",druidStatProperties.getLoginUsername());
+        initParams.put("loginPassword",druidStatProperties.getLoginPassword());
+        initParams.put("allow",druidStatProperties.getAllow());         //默认就是允许所有访问
+//         initParams.put("deny",druidStatProperties.getDeny());
+        servletRegistrationBean.setInitParameters(initParams);
+        return servletRegistrationBean;
+    }
+
+    //配置Druid后台监控的Filter
+    @Bean
+    @ConditionalOnProperty(name = "druid.stat.active")
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        Map<String,String> initParams = new HashMap<>();
+        initParams.put("exclusions",druidStatProperties.getFilterExclusions());
+        filterRegistrationBean.setInitParameters(initParams);
+        filterRegistrationBean.setUrlPatterns(druidStatProperties.getFilterUrlPatterns());
+        return filterRegistrationBean;
+    }
+
+}
---
+++ Felinae-AutoConfiguration/src/main/java/william/autoconfiguration/datasource/DruidStatProperties.java
@@ -0,0 +1,77 @@
+package william.autoconfiguration.datasource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+/**
+ * @Auther: ZhangShenao
+ * @Date: 2018/7/5 14:33
+ * @Description:Druid数据源监控属性
+ */
+@ConfigurationProperties(prefix = "druid.stat")
+public class DruidStatProperties {
+    private String loginUsername = "admin";
+    private String loginPassword = "123456";
+    private String allow = "";
+    private String deny = "";
+    private String servletUrlMappings = "/druid/*";
+    private List<String> filterUrlPatterns;
+    private String filterExclusions;
+
+    public String getLoginUsername() {
+        return loginUsername;
+    }
+
+    public void setLoginUsername(String loginUsername) {
+        this.loginUsername = loginUsername;
+    }
+
+    public String getLoginPassword() {
+        return loginPassword;
+    }
+
+    public void setLoginPassword(String loginPassword) {
+        this.loginPassword = loginPassword;
+    }
+
+    public String getAllow() {
+        return allow;
+    }
+
+    public void setAllow(String allow) {
+        this.allow = allow;
+    }
+
+    public String getDeny() {
+        return deny;
+    }
+
+    public void setDeny(String deny) {
+        this.deny = deny;
+    }
+
+    public String getServletUrlMappings() {
+        return servletUrlMappings;
+    }
+
+    public void setServletUrlMappings(String servletUrlMappings) {
+        this.servletUrlMappings = servletUrlMappings;
+    }
+
+    public List<String> getFilterUrlPatterns() {
+        return filterUrlPatterns;
+    }
+
+    public void setFilterUrlPatterns(List<String> filterUrlPatterns) {
+        this.filterUrlPatterns = filterUrlPatterns;
+    }
+
+    public String getFilterExclusions() {
+        return filterExclusions;
+    }
+
+    public void setFilterExclusions(String filterExclusions) {
+        this.filterExclusions = filterExclusions;
+    }
+}
